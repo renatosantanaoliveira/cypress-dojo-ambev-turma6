294bdcdde9732a1adc82813e25cd33784ee620f0
Renato Santana <santana@MacBook-Pro-de-Renato.local>
2022-11-18 15:24:37 -0300

    Subindo aula relatorios

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'iubfrs',
   e2e: {
     baseUrl: 'https://conexaoqa.herokuapp.com',
     setupNodeEvents(on, config) {
